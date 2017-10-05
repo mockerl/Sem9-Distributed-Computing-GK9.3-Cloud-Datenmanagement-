789190e3d9658faf9b881fa72afd7e5d12bb3d74
mockerl <kilian.mock@hotmail.com>
2017-10-05 16:02:58 +0200

    added urls

--- kilirest/urls.py
+++ kilirest/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^snippets/$', views.user_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.user_detail),
+    url(r'^user/$', views.user_list),
+    url(r'^user/(?P<pk>[0-9]+)/$', views.user_detail),
 ]
